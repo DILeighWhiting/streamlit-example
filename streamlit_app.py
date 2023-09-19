426bc1d398aa2cb8691b3478178f7b65a859ac83
DILeighWhiting <141292109+DILeighWhiting@users.noreply.github.com>
2023-09-19 11:04:35 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import streamlit as st
 # Decision Inc. Sales Demo for
 """
 
-st.image(https://www.maiafinancial.com.au/assets/static/logo-gradient-sm@2x.d1a35dd.a65badf05cf5b943cdbb5dbb6077a909.png)
+st.image("https://www.maiafinancial.com.au/assets/static/logo-gradient-sm@2x.d1a35dd.a65badf05cf5b943cdbb5dbb6077a909.png")
 
 """
 If you have any questions, reach out to our [sales team](mailto:salesau@decisioninc.com.au)
